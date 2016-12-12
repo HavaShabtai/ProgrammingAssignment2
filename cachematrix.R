43d56b790d160da3741c487ac9de0a2bd766d6c1
Hava <j.h.shabtai@gmail.com>
2016-12-12 16:41:10 +0100

    Update of the cachematrix function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+  ## Create a function which starts with a null matrix argument that 
+  ## store a matrix and its inverse
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(Mat = matrix()) {
+  inv <- matrix()
+  set <- function(y) {
+    Mat <<- y
+    inv <<- matrix()
+  }
+  get <- function() Mat
+  
+  ## Calculates the inverse of non-singular matrix via the solve function
+  
+  setinverse <- function(solve) inv <<- solve
+  
+  ## Gets the inverse
+  
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
+## A function to get the cache of inverse of a non-singular 
+## matrix via the solve function matrix
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(Mat, ...) {
+  inv <- Mat$getinverse()
+  
+  ## If the inverse already stored it retrieve the inverse
+  
+  if(!any(is.na(inv))) {
+    message("getting cached data")
+    return(inv)
+  }
+  
+  ## If the inverse is not stored, first the invers is calculated 
+  ## and then retrieved.
+  
+  data <- Mat$get()
+  inv <- solve(data, ...)
+  Mat$setinverse(inv)
+  inv
 }
